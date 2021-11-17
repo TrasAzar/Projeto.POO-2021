0ae0fb6092179f25d6cfbc72c3051830f2798479
TrasAzar <testemaximogit@gmail.com>
2021-11-16 21:49:26 -0300

    Pula-pula feito

--- Codigos/ativ-poo/pulapula.ts
+++ Codigos/ativ-poo/pulapula.ts
@@ -1,7 +1,7 @@
 class Kid{
-    id: String;
-    idade: Number;
-    constructor(id: String, idade: Number){
+    id: string;
+    idade: number;
+    constructor(id: string, idade: number){
         this.id = id;
         this.idade = idade
     }
@@ -10,25 +10,48 @@ class Kid{
 class pulapula{
     brincando : Array <Kid | null>
     esperando : Array <Kid | null>
-    constructor(qtdlugar:Number){
+    constructor(){
         this.brincando = []
         this.esperando = []
-        for (let i= 0;i < qtdlugar;i++){
-            this.brincando.push(null)
+        }
+    
+    entrada(kid: Kid){
+        this.esperando.push(kid)
+    }
+
+    in(){
+        this.brincando.push(this.esperando[0])
+        this.esperando =this.esperando.slice(1, this.esperando.length);
+    }
+    out(){
+        this.esperando.push(this.brincando[0])
+        this.brincando = this.brincando.slice(1, this.brincando.length);
+    }
+
+    removeKidE(id: string, vetor: Array <Kid>){
+        let index = 0;
+        let encontrou = false;
+        for(let k=0; k < vetor.length; k++){
+            if(vetor[k].id === id){
+                index = vetor.indexOf(vetor[k])
+                encontrou = true;
+            }
+        }
+        if(encontrou){
+            vetor.slice(index, 1)
+            return true;
+        }else{
+            return false;
         }
     }
 
-    entrada(kid: Kid){
-        let Kid
-    }
 
-    entra(){
-        if(qtdlugar <)
+toString(){
+    console.log("fila:");
+    let saida = ""
+    for(let n = 0; n < this.esperando.length; n++){
+        saida += this.esperando[n].id + ":" + this.esperando[n].idade + " "
     }
-    out(){
-        if(kid )
-    }
-
+    return saida;
 }
-
-toString
+}
