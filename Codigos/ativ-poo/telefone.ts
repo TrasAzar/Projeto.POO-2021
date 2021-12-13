fc0d5163639326ef25ff9d46f897eda3e8339c17
TrasAzar <testemaximogit@gmail.com>
2021-12-13 14:41:20 -0300

    Acrescimo da agenda

--- Codigos/ativ-poo/telefone.ts
+++ Codigos/ativ-poo/telefone.ts
@@ -1,4 +1,4 @@
-class   Fone{
+class  Fone{
     private id: string;
     private numero: string;
     constructor(id: string, numero: string){
@@ -48,3 +48,74 @@ class Contato{
         }
     }
 }
+
+class Agenda{
+    contatos: Map<string, Contato>;
+
+     Agenda(){
+    this.contatos = new Map<string, Contato>();
+     }
+
+    adicionaContato (contato: Contato){
+        if(this.contatos.has(contato.nome)){
+            let existente = this.contatos.get(contato.nome);
+            for(let fone of contato.fones){
+                existente.AddFone(fone);
+            }
+        }else{
+            this.contatos.set(contato.nome, contato);
+            }
+        }
+
+        acharContato(nome: string){
+            if(this.contatos.has(nome)){
+                return this.contatos.get(nome);
+            }
+            return null;
+        }
+
+        removerContato(nome: string){
+            if(this.contatos.delete(nome))
+            return true
+            // }else{
+            return false
+        }
+
+        Busca(padrao): Array<Contato> {
+            let result = new Array<Contato>();
+            for(let contato of this.contatos.values()){
+                if(contato.nome.includes(padrao)){
+                    result.push(contato);
+                }
+                return;
+            }
+
+        }
+        
+            
+        
+        lista(){
+            return this.contatos
+        }
+
+    }
+   
+    
+
+    // getContato(){
+    //     return this.id;
+    // }
+
+    // AddAgenda(fone: Fone, contato: Contato){
+    //     this.contato.get
+    // }
+
+   
+    // Agenda(){
+    // let label = new Map<number, string>();
+
+    // Contato.set();
+    // Contato.set();
+    // Contato.set();
+    // }
+    
