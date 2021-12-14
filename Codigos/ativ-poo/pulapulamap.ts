d508d2dcf8323a86ac59d094e4e7b51009af4b6a
TrasAzar <testemaximogit@gmail.com>
2021-12-14 05:28:30 -0300

    Tentando entender esse map

---
+++ Codigos/ativ-poo/pulapulamap.ts
@@ -0,0 +1,71 @@
+class Kid{
+    id: string;
+    idade: number;
+    constructor(id: string, idade: number){
+        this.id = id;
+        this.idade = idade
+    }
+}
+
+class pulapula{
+    brincando : Array <Kid | null>
+    esperando : Array <Kid | null>
+    constructor(){
+        this.brincando = []
+        this.esperando = []
+        }
+    
+    entrada(kid: Kid){
+        this.esperando.push(kid)
+    }
+
+    in(kid: Kid){
+        for( let entry of kid.entries()){
+            console.log(entry[0]. entry [1]);
+        }
+        // let idade = new Map<number, string>();
+        // if(this.brincando.has(esperando)){
+        //     let existente = this.brincando.get(esperando);
+        //     for(let brincando of esperando){
+        //         existente.AddFone();
+        //     }
+        // }else{
+        //     this.brincando.set(esperando);
+        //     }
+        // }
+        // this.brincando.push(this.esperando[0])
+        // this.esperando =this.esperando.slice(1, this.esperando.length);
+    }
+    out(){
+        
+        this.esperando.push(this.brincando[0])
+        this.brincando = this.brincando.slice(1, this.brincando.length);
+    }
+
+    removeKidE(id: string, vetor: Array <Kid>){
+        let index = 0;
+        let encontrou = false;
+        for(let k=0; k < vetor.length; k++){
+            if(vetor[k].id === id){
+                index = vetor.indexOf(vetor[k])
+                encontrou = true;
+            }
+        }
+        if(encontrou){
+            vetor.slice(index, 1)
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+toString(){
+    console.log("fila:");
+    let saida = ""
+    for(let n = 0; n < this.esperando.length; n++){
+        saida += this.esperando[n].id + ":" + this.esperando[n].idade + " "
+    }
+    return saida;
+}
+}
